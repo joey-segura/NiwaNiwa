85ea59e18991576ceb3394042092731066f227ed
Joey Segura <10735119@my.uvu.edu>
2020-12-05 12:26:02 -0700

    Stage SetUp, Swipe Speed / Distance changes

---
+++ Niwa-Niwa/Assets/Scripts/SwipeHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeHandler : MonoBehaviour
+{
+    public Swipe swipeControls;
+    public Transform player;
+    public bool isBlocked_0 = false;
+    public bool isBlocked_1 = false;
+    private Vector3 desiredPosition;
+
+    private void Update()
+    {
+        if (swipeControls.swipeLeft)
+            desiredPosition += Vector3.left;
+        if (swipeControls.swipeRight)
+            desiredPosition += Vector3.right;
+        if (swipeControls.swipeUp)
+            desiredPosition += Vector3.forward;
+        if (swipeControls.swipeDown)
+            desiredPosition += Vector3.back;
+        
+        player.transform.position = Vector3.MoveTowards(player.transform.position, desiredPosition * 3, 15f * Time.deltaTime);
+    }
+}
